7cc9aafedc80cc1059b9ae2b38f4e99d6432e5fc
Matt Lim <mlimcollege@gmail.com>
2022-10-20 16:51:27 -0400

    Upgrade Prisma, make first migration

---
+++ prisma/migrations/20221020205057_init/migration.sql
@@ -0,0 +1,51 @@
+-- CreateTable
+CREATE TABLE "currency" (
+    "id" UUID NOT NULL DEFAULT gen_random_uuid(),
+    "name" TEXT NOT NULL,
+    "decimals" INTEGER NOT NULL,
+    "symbol" TEXT NOT NULL,
+    "shortSymbol" TEXT,
+    "iconSrc" TEXT,
+    "mint" TEXT NOT NULL,
+    "usdRate" DECIMAL,
+    "solRate" DECIMAL,
+
+    CONSTRAINT "currency_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "flip" (
+    "id" UUID NOT NULL DEFAULT gen_random_uuid(),
+    "currencyId" UUID NOT NULL,
+    "userId" UUID NOT NULL,
+    "betAmount" BIGINT NOT NULL,
+    "flipsPrediction" INTEGER NOT NULL,
+    "flipsResult" INTEGER NOT NULL,
+    "timeCreated" TIMESTAMPTZ(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "flip_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "user" (
+    "id" UUID NOT NULL DEFAULT gen_random_uuid(),
+    "userAddress" TEXT NOT NULL,
+    "username" TEXT,
+
+    CONSTRAINT "user_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "currency_name_key" ON "currency"("name");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "currency_symbol_key" ON "currency"("symbol");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "currency_mint_key" ON "currency"("mint");
+
+-- AddForeignKey
+ALTER TABLE "flip" ADD CONSTRAINT "flip_currencyId_fkey" FOREIGN KEY ("currencyId") REFERENCES "currency"("id") ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+-- AddForeignKey
+ALTER TABLE "flip" ADD CONSTRAINT "flip_userId_fkey" FOREIGN KEY ("userId") REFERENCES "user"("id") ON DELETE RESTRICT ON UPDATE RESTRICT;
